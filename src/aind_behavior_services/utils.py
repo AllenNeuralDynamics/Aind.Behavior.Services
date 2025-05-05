af6354bae586bf22436ae1f7130a66944205af49
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-05-05 09:24:41 -0700

    Avoid deprecation warning from pydantic class method
    See https://github.com/pydantic/pydantic/pull/11169

--- src/aind_behavior_services/utils.py
+++ src/aind_behavior_services/utils.py
@@ -474,7 +474,7 @@ def get_fields_of_type(
     elif isinstance(searchable, list):
         _iterable = list(zip([None for _ in range(len(searchable))], searchable))
     elif isinstance(searchable, pydantic.BaseModel):
-        _iterable = {k: getattr(searchable, k) for k in searchable.model_fields.keys()}.items()
+        _iterable = {k: getattr(searchable, k) for k in type(searchable).model_fields.keys()}.items()
     else:
         raise ValueError(f"Unsupported model type: {type(searchable)}")
 
