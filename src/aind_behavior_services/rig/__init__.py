29eaea12f97d6db31c782c74ee9f1828069db48e
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-12-10 13:06:35 -0800

    Clean up import

--- src/aind_behavior_services/rig/__init__.py
+++ src/aind_behavior_services/rig/__init__.py
@@ -1,10 +1,6 @@
-from . import cameras, harp, visual_stimulation
 from ._base import AindBehaviorRigModel, Device
 
 __all__ = [
     "AindBehaviorRigModel",
     "Device",
-    "cameras",
-    "harp",
-    "visual_stimulation",
 ]
