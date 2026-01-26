4b232cf8901a2bebca056c4358e50427eba3e210
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-01-26 08:54:55 -0800

    Move schema generation to separate module

--- src/aind_behavior_services/__init__.py
+++ src/aind_behavior_services/__init__.py
@@ -3,6 +3,7 @@ import logging
 from ._version import __semver__, __version__
 from .base import DefaultAwareDatetime, SchemaVersionedModel
 from .rig import AindBehaviorRigModel
+from .schema import BonsaiSgenSerializers, convert_pydantic_to_bonsai
 from .session import AindBehaviorSessionModel
 from .task_logic import AindBehaviorTaskLogicModel
 
@@ -16,4 +17,6 @@ __all__ = [
     "DefaultAwareDatetime",
     "__version__",
     "__semver__",
+    "BonsaiSgenSerializers",
+    "convert_pydantic_to_bonsai",
 ]
