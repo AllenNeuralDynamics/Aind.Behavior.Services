5b07936a993a3be3d3030790bef695a754d517cc
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-03-14 01:37:13 -0700

    Add payloads and json-schema extension attribute

--- src/aind_behavior_services/data_types.py
+++ src/aind_behavior_services/data_types.py
@@ -1,10 +1,14 @@
 from enum import StrEnum
 from typing import Any, Generic, Literal, Optional, TypeVar
 
-from pydantic import BaseModel, ConfigDict, Field, SerializeAsAny
+from pydantic import AwareDatetime, BaseModel, ConfigDict, Field, SerializeAsAny
 
 from aind_behavior_services import __semver__
 from aind_behavior_services.base import SchemaVersionedModel
+
+from .schema import sgen_typename
+
+_SGEN_TYPENAME_NAMESPACE = "AllenNeuralDynamics.AindBehaviorServices.DataTypes"
 
 
 class DataType(StrEnum):
@@ -25,6 +29,7 @@ class TimestampSource(StrEnum):
 TData = TypeVar("TData", bound=Any)
 
 
+@sgen_typename(_SGEN_TYPENAME_NAMESPACE + ".SoftwareEvent")
 class SoftwareEvent(BaseModel, Generic[TData]):
     """
     A software event is a generic event that can be used to track any event that occurs in the software.
@@ -40,16 +45,29 @@ class SoftwareEvent(BaseModel, Generic[TData]):
     data_type_hint: Optional[str] = Field(default=None, description="The data type hint of the event")
 
 
+@sgen_typename(_SGEN_TYPENAME_NAMESPACE + ".RenderSynchState")
 class RenderSynchState(BaseModel):
     sync_quad_value: Optional[float] = Field(default=None, ge=0, le=1, description="The synchronization quad value")
     frame_index: Optional[int] = Field(default=None, ge=0, description="The frame index of the event")
     frame_timestamp: Optional[float] = Field(default=None, ge=0, description="The timestamp of the frame")
 
 
+@sgen_typename(_SGEN_TYPENAME_NAMESPACE + ".StartExperimentPayload")
+class StartExperimentPayload(BaseModel):
+    timestamp: AwareDatetime = Field(description="The start time of the session")
+
+
+@sgen_typename(_SGEN_TYPENAME_NAMESPACE + ".EndExperimentPayload")
+class EndExperimentPayload(BaseModel):
+    timestamp: AwareDatetime = Field(description="The end time of the session")
+
+
 class DataTypes(SchemaVersionedModel):
     version: Literal[__semver__] = __semver__
     software_event: SoftwareEvent
     render_synch_state: RenderSynchState
+    start_payload: StartExperimentPayload
+    end_payload: EndExperimentPayload
     model_config = ConfigDict(
         json_schema_extra={
             "x-abstract": "True",
